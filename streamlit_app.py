2b9f020f910861af5fe2182ffdbcb76f1c56d91b
teliadam <93702155+teliadam@users.noreply.github.com>
2022-12-22 16:34:57 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit
+import pandas
 
 streamlit.title('My Parents New Healthy Diner')
 
@@ -9,3 +10,6 @@ streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 streamlit.header('🥑🍞 Avocado Toast')
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
